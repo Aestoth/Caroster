2a0843a8428d4740ec8cbea27b5f1180d9a273b6
Anthony Claude <claude.anthony@bluewin.ch>
2019-06-25 16:16:09 +0200

    ajout de la page ajouter une voiture

--- src/AddVoiture.js
+++ src/AddVoiture.js
@@ -1,28 +1,48 @@
 import React, { Component } from "react";
-import {MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBFooter } from "mdbreact";
+import {MDBContainer, MDBRow, MDBCol, MDBBtn,MDBInput, MDBFooter } from "mdbreact";
 import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.css";
 
 
 class AddVoiture extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOpen: false,
-      inputValue: ""
+        nomVoiture: '',
+        sieges: '',
+        infocomp: '',
+        adresse: '',
+        date: new Date(),
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(value) {
-    console.log(value);
-    this.setState({ inputValue: value });
+  handleChange (event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({[name] : value})
   }
 
-  toggleCollapse = () => {
-    this.setState({ isOpen: !this.state.isOpen });
-  };
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch("http://localhost:3001/api/ajouter-voiture/new", {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Success" + data);
+      });
+    });
+  }
+
 
   render() {
     const Button = styled.button`
@@ -43,85 +63,33 @@ class AddVoiture extends Component {
         </nav>
         <div className="container-fluid cover-container d-flex">
           <div className="row col-12 align-items-center justify-content-center flex-fill mx-auto ">
-            <form className="mx-auto">
+            <form  onSubmit={this.handleSubmit} className="mx-auto">
               <div className="form-group col-12">
-                <MDBInput
-                  type="text"
-                  label="Nom de la voiture"
-                  icon="car"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-0"
+                <MDBInput type="text" label="Nom de la voiture"icon="car" className="mb-0"name="nomVoiture" value={this.state.nomVoiture}  onChange={this.handleChange}
                 />
               </div>
               <div className="form-group col-12">
-                <MDBInput
-                  type="number"
-                  label="Sièges"
-                  icon="chair"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-0"
-                />
+                <MDBInput type="number" label="Sieges" icon="chair" className="mb-0"name="sieges" value={this.state.sieges} onChange={this.handleChange} />
               </div>
               <div className="form-group col-12">
-                <MDBInput
-                  type="text"
-                  label="Notes"
-                  icon="pen"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-0"
-                />
+                <MDBInput type="text" label="Informations complémentaires" icon="pen" className="mb-0"name="infocomp" value={this.state.infocomp} onChange={this.handleChange} />
               </div>         
                 <h6 className="text-center text-uppercase mt-5">Lieu de rendez-vous</h6>
               <div className="form-group col-12">
-                <MDBInput
-                  type="text"
-                  label="Adresse"
-                  icon="map-marker-alt"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-0"
-                />
+                <MDBInput type="text" label="Adresse" icon="map-marker-alt" className="mb-0"name="adresse" value={this.state.adresse} onChange={this.handleChange} />
               </div>
               <div className="md-form form-group col-12">
-              <MDBInput
-                  type="date"
-                  icon="calendar"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-2"
-                />
+              <MDBInput type="date" icon="calendar" className="mb-2 textbox-n"name="date" value={this.state.date} onChange={this.handleChange} />
               </div>
               <div className="md-form form-group col-12">
-              <MDBInput
-                  type="time"
-                  icon="clock"
-                  format="hh:mm"
-                  group
-                  validate
-                  error="wrong"
-                  success="right"
-                  className="mb-2"
-                />
+              <MDBInput type="time" icon="clock" className="mb-2"name="heure" onChange={this.handleChange} />
               </div>
               
               <div className="text-center mt-2">
                 <MDBBtn className="text-uppercase text-white" type="cancel">
                   Annuler
                 </MDBBtn>
-                <Link to="/Evenement">
+                <Link to="/Evenement"> 
                   <MDBBtn className="text-uppercase text-white" type="submit">
                     Creer
                   </MDBBtn>
