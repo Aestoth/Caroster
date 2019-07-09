c61e520b6b32c03e57afe487f48341138ad7e5d7
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2019-07-09 16:41:14 +0200

    mise a jour

--- Frontend caroster/src/PassagersEnAttente.js
+++ Frontend caroster/src/PassagersEnAttente.js
@@ -36,39 +36,38 @@ class PassagersEnAttente extends Component {
   render() {
     return (
       <MDBContainer className="mt-3">
-        {this.state.showModifierPassager ? (
+        {this.state.showModifierPassager && (
           <ModifierPassager
             passagerModif={this.state.passagerModif}
             changeShowModifierPassager={() => this.changeShowModifierPassager()}
             fetchPassagers={this.props.fetchPassagers}
           />
-        ) : (
-          this.props.passagers.map(({ _id, nom }) => (
-            <MDBRow key={_id}>
-              <MDBCol size="5" className="mr-0 mt-2">
-                {" "}
-                <i className="fas fa-user pr-0 mr-1" />
-                {nom}
-              </MDBCol>
-              <MDBCol size="5" className="ml-0 ">
-                <select
-                  className="form-control mb-3"
-                  id="exampleFormControlSelect1"
-                >
-                  <option>Aller avec</option>
-                  <option>Voiture 1</option>
-                  <option>Voiture 2</option>
-                </select>
-              </MDBCol>
-              <MDBCol size="1" className="mt-2">
-                <MDBIcon
-                  icon="pencil-alt"
-                  onClick={() => this.ModifierPassagerShow(_id)}
-                />
-              </MDBCol>
-            </MDBRow>
-          ))
         )}
+        {this.props.passagers.map(({ _id, nom }) => (
+          <MDBRow key={_id}>
+            <MDBCol size="5" className="mr-0 mt-2">
+              {" "}
+              <i className="fas fa-user pr-0 mr-1" />
+              {nom}
+            </MDBCol>
+            <MDBCol size="5" className="ml-0 ">
+              <select
+                className="form-control mb-3"
+                id="exampleFormControlSelect1"
+              >
+                <option>Aller avec</option>
+                <option>Voiture 1</option>
+                <option>Voiture 2</option>
+              </select>
+            </MDBCol>
+            <MDBCol size="1" className="mt-2">
+              <MDBIcon
+                icon="pencil-alt"
+                onClick={() => this.ModifierPassagerShow(_id)}
+              />
+            </MDBCol>
+          </MDBRow>
+        ))}
       </MDBContainer>
     );
   }
